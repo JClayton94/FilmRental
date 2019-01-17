e8406ca2d4ba31c87b3f192c238bce3a0586fbff
JClayton94 <joshclaytonmarrs@gmail.com>
2019-01-17 17:00:10 +0000

    updated, bootstrap needed

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
 
 //console.log("request.response[0]");
 
-function getHTTP(){
+let JSONArray = [];
+
+function getData(){
     var requestURL = 'http://127.0.0.1:8080/api/film';
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
@@ -11,29 +13,23 @@ function getHTTP(){
     request.send();
    
     request.onload = function() {
-        for(i=0; i < request.response.length; i++){
-        document.getElementById('textArea').innerHTML += request.response[i].title + " <br>";
-        }
+       
+        JSONArray = request.response;
+
     }
 }
 function searchFilms(film){
-    var requestURL = 'http://127.0.0.1:8080/api/film';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.setRequestHeader('Access-Control-Allow-Origin', '*');
-    request.responseType = 'json'
-    request.send();
-    request.onload = function() {
-        document.getElementById('textArea').innerHTML = "";
-        for(i=0; i < request.response.length; i++){
-            if(request.response[i].title.includes(film.value.toUpperCase())){
 
-                document.getElementById('textArea').innerHTML += request.response[i].title + " <br>" + request.response[i].description  +" <br>";
+    document.getElementById('textArea').innerHTML = "";
+    if(film.value == ''){
+        return;
+    }
+    for(i=0; i < JSONArray.length; i++){
+        
+        if(JSONArray[i].title.includes(film.value.toUpperCase())){
 
-            }
+            document.getElementById('textArea').innerHTML += JSONArray[i].title + " <br>" + JSONArray[i].description  +" <br>";
 
         }
-        //document.getElementById('textArea').innerHTML = "Not found!";
     }
 }
